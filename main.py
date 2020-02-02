148a9f03e7979130d40db3bf03ede4fec615839a
jmcard <43482871+jmcard@users.noreply.github.com>
2020-02-01 23:32:34 -0300

    alimentar

--- main.py
+++ main.py
@@ -2,43 +2,41 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((500, 500))
 
 pygame.display.set_caption("Pet")
 
 char = pygame.image.load('yoda2.png')
-bg = pygame.image.load('forest.png')
+# bg = pygame.image.load('')
 barra1 = pygame.image.load('barra1.png')
 barra2 = pygame.image.load('barra1.png')
 fome = pygame.image.load('fome.png')
 energia = pygame.image.load('energia.png')
+comida = pygame.image.load('comida.png')
 
-x = 100
-y = 100
-width = 50
-height = 50
-vel = 5
+fome_width = 0
 
 def janela():
     # win.blit(bg, (0,0))
     # win.blits(bg, (1))
-    win.fill((255,255,255))
-    win.blit(char, (35,20))
+    win.fill((255, 255, 255))
+    win.blit(char, (35, 20))
 
     win.blit(barra1, (90, 10))
-    win.blit(fome, (115,30))
+    win.blit(fome, (115, 30))
     win.blit(barra2, (300, 10))
     win.blit(energia, (320, 30))
+    win.blit(comida, (10, 470))
 
-
-    pygame.draw.rect(win, (51,204,51), (93,25 ,20, 15))
+    pygame.draw.rect(win, (51, 204, 51), (93, 25, fome_width, 15))
 
     pygame.display.update()
 
 
 run = True
 while run:
-    pygame.time.delay(100)
+
+    pygame.time.delay(50)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -46,7 +44,11 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    # if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
+        if fome_width <= 110:
+            fome_width = fome_width + 5
+        janela()
+        print('Alimentado')
 
     janela()
 pygame.quit()
